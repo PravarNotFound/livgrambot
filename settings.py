4908f67cad21d6d844e504caf32866852afebbc9
PravarNotFound <105976832+PravarNotFound@users.noreply.github.com>
2022-07-03 14:49:35 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,6 +17,20 @@ if TELEGRAM_SUPPORT_CHAT_ID is None or not str(TELEGRAM_SUPPORT_CHAT_ID).lstrip(
 TELEGRAM_SUPPORT_CHAT_ID = int(TELEGRAM_SUPPORT_CHAT_ID)
 
 
-WELCOME_MESSAGE = os.getenv("WELCOME_MESSAGE", "👋")
+WELCOME_MESSAGE = os.getenv("WELCOME_MESSAGE", "**Hey buddy** [👋](https://te.legra.ph/file/88a636de87d2c2583d469.mp4)
+
+⛔ **You can contact with us using this bot.**
+
+⭕ 𝗥𝗘𝗠𝗘𝗠𝗕𝗘𝗥 
+   **• No invite links**
+   **• No selling**
+   **• No bot refferal links**
+   **• No abusing**
+
+**Jᴜꜱᴛ Dʀᴏᴘ Yᴏᴜʀ Mᴇꜱꜱᴀɢᴇ Hᴇʀᴇ** ↙️⤵️
+
+[📛 𝑱𝑶𝑰𝑵 𝑶𝑼𝑹 𝑶𝑭𝑭𝑰𝑪𝑰𝑨𝑳 𝑮𝑹𝑶𝑼𝑷 📛](https://t.me/+gSrp0a_3QAliNzM9)
+
+")
 REPLY_TO_THIS_MESSAGE = os.getenv("REPLY_TO_THIS_MESSAGE", "REPLY_TO_THIS")
 WRONG_REPLY = os.getenv("WRONG_REPLY", "WRONG_REPLY")
